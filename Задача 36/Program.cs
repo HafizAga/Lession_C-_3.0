cd443607b20055c0b5eae4fa5ad7b4192dfeec31
HafiAga <ulguiorra2@gmail.com>
2022-12-05 22:00:44 +0300

    Задача 36.

--- Задача 36/Program.cs
+++ Задача 36/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.WriteLine("Введите размер массива");
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
